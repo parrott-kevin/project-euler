19c651de905103cb993288e57ad8b54afed1e90c
Kevin Parrott <parrott.kevin@gmail.com>
2015-06-13 01:42:30 -0400

    Fixed duplicate declaration error

--- public/src/app/components/problems/problems.controller.js
+++ public/src/app/components/problems/problems.controller.js
@@ -8,27 +8,27 @@
   ProblemsController.$inject = ['problem1', 'problem2', 'problem3'];
   function ProblemsController(problem1, problem2, problem3) {
     var vm = this;
-    var problem1 = problem1.get();
-    var problem2 = problem2.get();
-    var problem3 = problem3.get();
+    var p1 = problem1.get();
+    var p2 = problem2.get();
+    var p3 = problem3.get();
     vm.problems = [
       {
         number: '1',
         title: 'Multiples of 3 and 5',
-        answer: problem1.result,
-        timer: problem1.timer
+        answer: p1.result,
+        timer: p1.timer
       },
       {
         number: '2',
         title: 'Even Fibonacci numbers',
-        answer: problem2.result,
-        timer: problem2.timer
+        answer: p2.result,
+        timer: p2.timer
       },
       {
         number: '3',
         title: 'Largest prime factor',
-        answer: problem3.result,
-        timer: problem3.timer
+        answer: p3.result,
+        timer: p3.timer
       }
 
     ];
